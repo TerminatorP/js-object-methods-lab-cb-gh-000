9b7861ad31d60bee8801fa494feaf45ff6b57e03
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-15 03:17:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code solution here
 class President {
-  constructor(name, homestate, politicalParty, yearsInOffice) {
+  constructor(name, politicalParty, yearsInOffice, homestate) {
     this.name = name;
     this.politicalParty = politicalParty;
     this.yearsInOffice = yearsInOffice;
